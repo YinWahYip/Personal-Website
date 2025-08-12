a7000b61b41ce52aac19800d04972efef32fd998
YinWahYip <yyip@villanova.edu>
2025-08-11 23:08:06 -0400

    I DID THE DOCUMENTATION YAY TIME TO FINISH IT

--- personal-website/src/Documentation/Documentation.js
+++ personal-website/src/Documentation/Documentation.js
@@ -8,27 +8,35 @@ function DocPage() {
                 <h2>
                     Because an <i>About me</i> would be soooooo much more "interesting"
                 </h2>
-                <a href="#info-box" className="scroll-arrow">↓</a>
+                <a href="#all-info" className="scroll-arrow">↓</a>
                 <div className="scroll-space" />
 
-                <div id="info-box" class="info-box-1">
+                <div id="all-info" class="all-info">
                     <div class="item info-block-1">
-                        <h1>Yin Wah Yip</h1>
-                        <h2><code>birth()</code></h2>
+                        <h1><code>.birth()</code></h1>
+
                         <p>The <code>birth()</code> method returns 3 integers: Date, Month, Year</p>
-                        <p><code>yinwah.birth()</code> returns: <br></br> </p>
-
+                        <p><code>yinwah.birth()</code> returns:<br /> <code>4/30/2003</code></p>
                     </div>
-
                     <div class="item headshot-1"></div>
                     <div class="item item-1"></div>
-                </div>
 
-                <div id="info-box2" class="info-box2">
-                    <div class="item info-block-2"></div>
+                    <br></br>
+
+
+                    <div class="item info-block-2">
+                        <h1><code>.sleep()</code></h1>
+                        <p><code>sleep()</code> takes in 2 parameters of type integer</p>
+                        <p><code>yinwah.birth()</code> returns:<br /> <code>4/30/2003</code></p>
+                    </div>
                     <div class="item headshot-2"></div>
                     <div class="item item-2"></div>
+
+                    <div class="item food-block"></div>
+                    <div class="item headshot-3"></div>
+                    <div class="item item-3"></div>
                 </div>
+
 
                 <div className="scroll-space" />
 
